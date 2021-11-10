573cdc749c4f6d616cf87baf2913fca9d2f9e1cd
elineri <elin.ericstam@gmail.com>
2021-11-10 15:48:53 +0100

    Småjusteringar.

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Elin Ericstam SUT21
+using System;
 
 namespace Polymorphism
 {
@@ -6,11 +7,11 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
-            Drawing s1 = new Square();
-            Console.WriteLine(s1.Area()); 
-
             Drawing c1 = new Circle();
             Console.WriteLine(c1.Area());
+
+            Drawing s1 = new Square();
+            Console.WriteLine(s1.Area()); 
 
             Drawing r1 = new Rectangle();
             Console.WriteLine(r1.Area());
